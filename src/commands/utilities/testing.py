114c3d3e5ba4b1fcce5c795d0eabe2324096f0e4
David Long <david.long@snows.world>
2024-03-18 19:05:15 -0400

    Ping command replies successfully

---
+++ src/commands/utilities/testing.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+class Testing(commands.Cog):
+    """
+    Tools for testing Makishima during development.
+    """
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @discord.app_commands.command()
+    async def ping(self, interaction: discord.Interaction):
+        try:
+            await interaction.response.send_message("Pong!")
+        except discord.HTTPException as err:
+            print(err)
+
+
+async def setup(makishima: commands.Bot):
+    await makishima.add_cog(Testing(makishima), guilds=makishima.guilds)
--- src/makishima.py
+++ src/makishima.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+from pathlib import Path
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
-load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,10 +12,36 @@ intents.message_content = True
 makishima = commands.Bot("./", intents=intents)
 
 
+async def load_commands(command_path: os.PathLike):
+    for entry in os.scandir(command_path):
+        name: str = entry.name
+
+        if entry.is_dir():
+            await load_commands(os.path.join(command_path, name))
+            continue
+
+        if not name.endswith(".py"):
+            continue
+
+        # continue with command import
+        print(f"Loading commands from {name}")
+        await makishima.load_extension(f"commands.{Path(command_path).name.replace("/", ".")}.{name[:-3]}")
+
 @makishima.event
 async def on_ready():
     activity = discord.CustomActivity("Reading classical literature")
-    makishima.change_presence(activity=activity, status=discord.Status.do_not_disturb)
+    await makishima.change_presence(activity=activity, status=discord.Status.do_not_disturb)
+
+    # load commands
+    await load_commands("src/commands")
+
+    for guild in makishima.guilds:
+        print(f"Synced commands in guild with ID {guild.id}")
+        await makishima.tree.sync(guild=guild)
+
+    print("Loading commands completed")
 
 
-makishima.run(os.environ["MAKISHIMA_TOKEN"])
+if __name__ == "__main__":
+    load_dotenv()
+    makishima.run(os.environ["MAKISHIMA_TOKEN"])
